0952be512149e80fd6097d62c2bc6df23c6f6f44
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2022-12-28 21:55:55 +0100

    Further steps towards the DB model.

--- prototyping/dbmodel/model.py
+++ prototyping/dbmodel/model.py
@@ -15,6 +15,12 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
+class MeansOfTransport(Base):
+    __tablename__ = "MEANS_OF_TRANSPORT"
+    id = Column("ID", Integer, primary_key=True)
+    identifier = Column("IDENTIFIER", String(length=20), nullable=False)
+
+
 class Node(Base):
     __tablename__ = "NODES"
     id = Column("ID", Integer, primary_key=True)
@@ -27,9 +33,22 @@ class Edge(Base):
     distance_min = Column("DISTANCE_MIN", Integer, nullable=False)
     start_node_id = Column("START_NODE_ID", Integer, ForeignKey("NODES.ID"), nullable=False)
     end_node_id = Column("END_NODE_ID", Integer, ForeignKey("NODES.ID"), nullable=False)
+    means_of_transport_id = Column("MEANS_OF_TRANSPORT_ID", Integer, ForeignKey("MEANS_OF_TRANSPORT.ID"), nullable=False)
     start_node = relationship("Node", foreign_keys="Edge.start_node_id")
     end_node = relationship("Node", foreign_keys="Edge.end_node_id")
+    means_of_transport = relationship("MeansOfTransport", foreign_keys="Line.means_of_transport_id")
 
+
+class Line(Base):
+    __tablename__ = "LINES"
+    id = Column("ID", Integer, primary_key=True, index=True)
+    means_of_transport_id = Column("MEANS_OF_TRANSPORT_ID", Integer, ForeignKey("MEANS_OF_TRANSPORT.ID"), nullable=False)
+    label = Column("LABEL", String, nullable=False)
+    terminal_stop_one_id = Column("TERMINAL_STOP_ONE_ID", Integer, ForeignKey("NODES.ID"), nullable=False)
+    terminal_stop_two_id = Column("TERMINAL_STOP_TWO_ID", Integer, ForeignKey("NODES.ID"), nullable=False)
+    means_of_transport = relationship("MeansOfTransport", foreign_keys="Line.means_of_transport_id")
+    terminal_stop_one = relationship("Node", foreign_keys="Line.terminal_stop_one_id")
+    terminal_stop_two = relationship("Node", foreign_keys="Line.terminal_stop_two_id")
 
 
 def main() -> None:
