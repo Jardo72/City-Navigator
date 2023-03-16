bed86e80ab454589025d1f6e47fe347780e71782
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2023-03-16 10:50:35 +0100

    First API endpoints of master data service starting to work.

--- application/master-data-service/main.py
+++ application/master-data-service/main.py
@@ -1,8 +1,11 @@
 from typing import List
 from uuid import uuid4
 
-from fastapi import FastAPI, status
+from fastapi import Depends, FastAPI, status
+from sqlalchemy.orm import Session
 
+from db import MeansOfTransport, Station
+from db import get_db
 from model import MeansOfTransportDetails, MeansOfTransportRequest
 from model import StationDetails, StationRequest
 
@@ -11,37 +14,19 @@ app = FastAPI()
 
 
 @app.get("/means-of-transport", response_model=List[MeansOfTransportDetails])
-async def get_means_of_transport_list():
-    return [
-        MeansOfTransportDetails(
-            uuid=uuid4(),
-            identifier="S-Bahn"
-        ),
-        MeansOfTransportDetails(
-            uuid=uuid4(),
-            identifier="U-Bahn"
-        ),
-        MeansOfTransportDetails(
-            uuid=uuid4(),
-            identifier="Tram"
-        ),
-        MeansOfTransportDetails(
-            uuid=uuid4(),
-            identifier="Bus"
-        )
-    ]
+async def get_means_of_transport_list(db: Session = Depends(get_db)):
+    result_set = db.query(MeansOfTransport).order_by(MeansOfTransport.identifier).all()
+    return [MeansOfTransportDetails(uuid=record.uuid, identifier=record.identifier) for record in result_set]
 
 
 @app.get("/means-of-transport/{uuid}", response_model=MeansOfTransportDetails)
-async def get_means_of_transport(uuid: str):
-    return MeansOfTransportDetails(
-        uuid=uuid4(),
-        identifier="S-Bahn"
-    )
+async def get_means_of_transport(uuid: str, db: Session = Depends(get_db)):
+    record = db.query(MeansOfTransport).filter(MeansOfTransport.uuid == uuid).first()
+    return MeansOfTransportDetails(uuid=record.uuid, identifier=record.identifier)
 
 
 @app.post("/means-of-transport/", response_model=MeansOfTransportDetails, status_code=status.HTTP_201_CREATED)
-async def create_means_of_transport(request: MeansOfTransportRequest):
+async def create_means_of_transport(request: MeansOfTransportRequest, db: Session = Depends(get_db)):
     return MeansOfTransportDetails(
         uuid=uuid4(),
         identifier=request.identifier
@@ -49,7 +34,7 @@ async def create_means_of_transport(request: MeansOfTransportRequest):
 
 
 @app.put("/means-of-transport/{uuid}")
-async def update_means_of_transport(request: MeansOfTransportRequest):
+async def update_means_of_transport(request: MeansOfTransportRequest, db: Session = Depends(get_db)):
     return MeansOfTransportDetails(
         uuid=uuid4(),
         identifier=request.identifier
@@ -57,18 +42,20 @@ async def update_means_of_transport(request: MeansOfTransportRequest):
 
 
 @app.delete("/means-of-transport/{uuid}")
-async def delete_means_of_transport(uuid: str):
+async def delete_means_of_transport(uuid: str, db: Session = Depends(get_db)):
     ...
 
 
 @app.get("/stations", response_model=List[StationDetails])
-async def get_station_list():
-    ...
+async def get_station_list(db: Session = Depends(get_db)):
+    result_set = db.query(Station).order_by(Station.name).all()
+    return [StationDetails(uuid=record.uuid, name=record.name) for record in result_set]
 
 
 @app.get("/station/{uuid}", response_model=StationDetails)
-async def get_station(uuid: str):
-    ...
+async def get_station(uuid: str, db: Session = Depends(get_db)):
+    record = db.query(Station).filter(Station.uuid == uuid).first()
+    return StationDetails(uuid=record.uuid, name=record.name)
 
 
 @app.post("/station", response_model=StationDetails, status_code=status.HTTP_201_CREATED)
