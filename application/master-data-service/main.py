3317a1e3ebc4bd4a961f1daacac17513ab59ce05
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2023-03-16 12:53:27 +0100

    Further API endpoints of master data service starting to work.

--- application/master-data-service/main.py
+++ application/master-data-service/main.py
@@ -1,7 +1,8 @@
 from typing import List
 from uuid import uuid4
 
-from fastapi import Depends, FastAPI, status
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi import status
 from sqlalchemy.orm import Session
 
 from db import MeansOfTransport, Station
@@ -27,14 +28,19 @@ async def get_means_of_transport(uuid: str, db: Session = Depends(get_db)):
 
 @app.post("/means-of-transport/", response_model=MeansOfTransportDetails, status_code=status.HTTP_201_CREATED)
 async def create_means_of_transport(request: MeansOfTransportRequest, db: Session = Depends(get_db)):
+    means_of_transport = MeansOfTransport()
+    means_of_transport.uuid = str(uuid4())
+    means_of_transport.identifier = request.identifier
+    db.add(means_of_transport)
+    db.commit()
     return MeansOfTransportDetails(
-        uuid=uuid4(),
+        uuid=means_of_transport.uuid,
         identifier=request.identifier
     )
 
 
 @app.put("/means-of-transport/{uuid}")
-async def update_means_of_transport(request: MeansOfTransportRequest, db: Session = Depends(get_db)):
+async def update_means_of_transport(uuid: str, request: MeansOfTransportRequest, db: Session = Depends(get_db)):
     return MeansOfTransportDetails(
         uuid=uuid4(),
         identifier=request.identifier
@@ -43,7 +49,14 @@ async def update_means_of_transport(request: MeansOfTransportRequest, db: Sessio
 
 @app.delete("/means-of-transport/{uuid}")
 async def delete_means_of_transport(uuid: str, db: Session = Depends(get_db)):
-    ...
+    record = db.query(MeansOfTransport).filter(MeansOfTransport.uuid == uuid).first()
+    if record is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Means of transport with the UUID {uuid} not found."
+        )
+    db.delete(record)
+    db.commit()
 
 
 @app.get("/stations", response_model=List[StationDetails])
@@ -55,19 +68,33 @@ async def get_station_list(db: Session = Depends(get_db)):
 @app.get("/station/{uuid}", response_model=StationDetails)
 async def get_station(uuid: str, db: Session = Depends(get_db)):
     record = db.query(Station).filter(Station.uuid == uuid).first()
+    if record is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Station with the UUID {uuid} not found."
+        )
     return StationDetails(uuid=record.uuid, name=record.name)
 
 
 @app.post("/station", response_model=StationDetails, status_code=status.HTTP_201_CREATED)
-async def create_station(request: StationRequest):
-    ...
+async def create_station(request: StationRequest, db: Session = Depends(get_db)):
+    station = Station()
+    station.uuid = str(uuid4())
+    station.name = request.name
+    db.add(station)
+    db.commit()
+    return StationDetails(uuid=station.uuid, name=station.name)
 
 
-@app.put("/station", response_model=StationDetails)
-async def update_station(request: StationRequest):
-    ...
+@app.put("/station/{uuid}", response_model=StationDetails)
+async def update_station(uuid: str, request: StationRequest, db: Session = Depends(get_db)):
+    record = db.query(Station).filter(Station.uuid == uuid).first()
+    record.name = request.name
+    db.commit()
 
 
 @app.delete("/station/{uuid}")
-async def delete_station(uuid: str):
-    ...
+async def delete_station(uuid: str, db: Session = Depends(get_db)):
+    record = db.query(Station).filter(Station.uuid == uuid).first()
+    db.delete(record)
+    db.commit()
