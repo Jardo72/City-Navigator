b724c4235609a1b121bd5c98bf292cf90c272b08
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2023-04-02 18:58:05 +0200

    Minor improvements.

---
+++ application/master-data-service/src/config.py
@@ -0,0 +1,37 @@
+#
+# Copyright 2023 Jaroslav Chmurny
+#
+# This file is part of City Navigator.
+#
+# City Navigator is free software developed for educational and experimental
+# purposes. It is licensed under the Apache License, Version 2.0 # (the
+# "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from logging import getLogger
+from os import environ
+
+
+_logger = getLogger("config")
+
+
+class Config:
+
+    @staticmethod
+    def _get_environment_variable(name: str, default_value: str) -> str:
+        result = environ.get(name, None)
+        default_used = False
+        if result is None:
+            default_used = True
+            result = default_value
+        _logger.info("Config - env. var = '%s', result = '%s', default used = %s", name, result, default_used)
+        return result
