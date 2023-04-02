87a7f7250d41b63ed0ad73493f891527f7e5b715
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2023-04-02 08:54:12 +0200

    Further steps towards restructuring of master data service.

--- application/master-data-service/rest/dto.py
+++ application/master-data-service/rest/dto.py
@@ -16,3 +16,57 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+from typing import List
+
+from pydantic import BaseModel
+
+
+class MeansOfTransportDetails(BaseModel):
+    uuid: str = None
+    identifier: str = None
+    presentation_color: str = None
+
+
+class MeansOfTransportRequest(BaseModel):
+    identifier: str = None
+    presentation_color: str = None
+
+
+class StationDetails(BaseModel):
+    uuid: str = None
+    name: str = None
+
+
+class StationRequest(BaseModel):
+    name: str = None
+
+
+class LineInfo(BaseModel):
+    uuid: str = None
+    label: str = None
+    means_of_transport: str = None
+    terminal_stop_one: str = None
+    terminal_stop_two: str = None
+
+
+class ItineraryEntry(BaseModel):
+    station: StationDetails = None
+    point_in_time_minutes: int = None
+
+
+class LineDetails(BaseModel):
+    uuid: str = None
+    label: str = None
+    means_of_transport: MeansOfTransportDetails = None
+    terminal_stop_one: StationDetails = None
+    terminal_stop_two: StationDetails = None
+    direction_one_itinerary: List[ItineraryEntry] = None
+    direction_two_itinerary: List[ItineraryEntry] = None
+
+
+class LineRequest(BaseModel):
+    label: str = None
+    means_of_transport_uuid: str = None
+    terminal_stop_one_uuid: str = None
+    terminal_stop_two_uuid: str = None
---
+++ application/master-data-service/rest/router.py
@@ -0,0 +1,201 @@
+#
+# Copyright 2023 Jaroslav Chmurny
+#
+# This file is part of City Navigator.
+#
+# City Navigator is free software developed for educational and experimental
+# purposes. It is licensed under the Apache License, Version 2.0 # (the
+# "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from typing import List
+from uuid import uuid4
+
+from fastapi import Depends, APIRouter, status
+from sqlalchemy.orm import Session
+
+from db import Line, MeansOfTransport, Station
+from db import get_db
+
+from .dto import LineDetails, LineInfo, LineRequest
+from .dto import MeansOfTransportDetails, MeansOfTransportRequest
+from .dto import StationDetails, StationRequest
+from .errors import line_not_found_exception, means_of_transport_not_found_exception, station_not_found_exception
+from .mapping import as_line_details, as_line_info, as_means_of_transport, as_station_details
+
+
+router = APIRouter()
+
+
+@router.get("/means-of-transport", response_model=List[MeansOfTransportDetails])
+async def get_means_of_transport_list(db: Session = Depends(get_db)):
+    """
+    Provides a list with details of all means of transport.
+    """
+    result_set = db.query(MeansOfTransport).order_by(MeansOfTransport.identifier).all()
+    return [as_means_of_transport(record) for record in result_set]
+
+
+@router.get("/means-of-transport/{uuid}", response_model=MeansOfTransportDetails)
+async def get_means_of_transport(uuid: str, db: Session = Depends(get_db)):
+    """
+    Provides the details of the means of transport with the given UUID.
+    """
+    record = db.query(MeansOfTransport).filter(MeansOfTransport.uuid == uuid).first()
+    if record is None:
+        raise means_of_transport_not_found_exception(uuid)
+    return as_means_of_transport(record)
+
+
+@router.post("/means-of-transport/", response_model=MeansOfTransportDetails, status_code=status.HTTP_201_CREATED)
+async def create_means_of_transport(request: MeansOfTransportRequest, db: Session = Depends(get_db)):
+    means_of_transport = MeansOfTransport()
+    means_of_transport.uuid = str(uuid4())
+    means_of_transport.identifier = request.identifier
+    db.add(means_of_transport)
+    db.commit()
+    return MeansOfTransportDetails(
+        uuid=means_of_transport.uuid,
+        identifier=request.identifier
+    )
+
+
+@router.put("/means-of-transport/{uuid}")
+async def update_means_of_transport(uuid: str, request: MeansOfTransportRequest, db: Session = Depends(get_db)):
+    record = db.query(MeansOfTransport).filter(MeansOfTransport.uuid == uuid).first()
+    if record is None:
+        raise means_of_transport_not_found_exception(uuid)
+    return MeansOfTransportDetails(
+        uuid=uuid4(),
+        identifier=request.identifier
+    )
+
+
+@router.delete("/means-of-transport/{uuid}")
+async def delete_means_of_transport(uuid: str, db: Session = Depends(get_db)):
+    """
+    Deletes the means of transport with the given UUID.
+    """
+    record = db.query(MeansOfTransport).filter(MeansOfTransport.uuid == uuid).first()
+    if record is None:
+        raise means_of_transport_not_found_exception(uuid)
+    db.delete(record)
+    db.commit()
+
+
+@router.get("/stations", response_model=List[StationDetails])
+async def get_station_list(db: Session = Depends(get_db)):
+    """
+    Provides a list with details of all stations. The returned stations are sorted by the name,
+    the order is ascending.
+    """
+    result_set = db.query(Station).order_by(Station.name).all()
+    return [as_station_details(record) for record in result_set]
+
+
+@router.get("/station/{uuid}", response_model=StationDetails)
+async def get_station(uuid: str, db: Session = Depends(get_db)):
+    """
+    Provides the details of the station with the given UUID.
+    """
+    record = db.query(Station).filter(Station.uuid == uuid).first()
+    if record is None:
+        raise station_not_found_exception(uuid)
+    return as_station_details(record)
+
+
+@router.post("/station", response_model=StationDetails, status_code=status.HTTP_201_CREATED)
+async def create_station(request: StationRequest, db: Session = Depends(get_db)):
+    station = Station()
+    station.uuid = str(uuid4())
+    station.name = request.name
+    db.add(station)
+    db.commit()
+    return StationDetails(uuid=station.uuid, name=station.name)
+
+
+@router.put("/station/{uuid}", response_model=StationDetails)
+async def update_station(uuid: str, request: StationRequest, db: Session = Depends(get_db)):
+    record = db.query(Station).filter(Station.uuid == uuid).first()
+    if record is None:
+        raise station_not_found_exception(uuid)
+    record.name = request.name
+    db.commit()
+    return as_station_details(record)
+
+
+@router.delete("/station/{uuid}")
+async def delete_station(uuid: str, db: Session = Depends(get_db)):
+    """
+    Deletes the station with the given UUID.
+    """
+    record = db.query(Station).filter(Station.uuid == uuid).first()
+    if record is None:
+        raise station_not_found_exception(uuid)
+    db.delete(record)
+    db.commit()
+
+
+@router.get("/lines", response_model=List[LineInfo])
+async def get_lines(db: Session = Depends(get_db)):
+    result_set = db.query(Line).order_by(Line.label).all()
+    return [as_line_info(record) for record in result_set]
+
+
+@router.get("/line/{uuid}", response_model=LineDetails)
+async def get_line(uuid: str, db: Session = Depends(get_db)):
+    """
+    Provides the details of the line with the given UUID.
+    """
+    record = db.query(Line).filter(Line.uuid == uuid).first()
+    if record is None:
+        raise line_not_found_exception(uuid)
+    return as_line_details(record)
+
+
+@router.post("/line", response_model=LineDetails, status_code=status.HTTP_201_CREATED)
+async def create_line(request: LineRequest, db: Session = Depends(get_db)):
+    line = Line()
+    line.uuid = str(uuid4())
+    line.label = request.label
+    line.means_of_transport_uuid = request.means_of_transport_uuid
+    line.terminal_stop_one_uuid = request.terminal_stop_one_uuid
+    line.terminal_stop_two_uuid = request.terminal_stop_two_uuid
+    db.add(line)
+    db.commit()
+    return as_line_details(line)
+
+
+@router.put("/line/{uuid}", response_model=LineDetails)
+async def update_line(uuid: str, request: LineRequest, db: Session = Depends(get_db)):
+    record = db.query(Line).filter(Line.uuid == uuid).first()
+    if record is None:
+        raise line_not_found_exception(uuid)
+    record.label = request.label
+    record.means_of_transport_uuid = request.means_of_transport_uuid
+    record.terminal_stop_one_uuid = request.terminal_stop_one_uuid
+    record.terminal_stop_two_uuid = request.terminal_stop_two_uuid
+    db.commit()
+    return as_line_details(record)
+
+
+@router.delete("/line/{uuid}")
+async def delete_line(uuid: str, db: Session = Depends(get_db)):
+    """
+    Deletes the line with the given UUID. This operation also deletes the itinerary
+    for the concerned line (both directions).
+    """
+    record = db.query(Line).filter(Line.uuid == uuid).first()
+    if record is None:
+        raise line_not_found_exception(uuid)
+    db.delete(record)
+    db.commit()
