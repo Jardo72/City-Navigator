fc5e89bac42e7dc07db7c3ae9f8bcff8eb229632
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2023-04-04 21:19:15 +0200

    Further steps towards test automation.

---
+++ test-automation/non-functional/abstract_client.py
@@ -0,0 +1,50 @@
+#
+# Copyright 2023 Jaroslav Chmurny
+#
+# This file is part of City Navigator.
+#
+# City Navigator is free software developed for educational and experimental
+# purposes. It is licensed under the Apache License, Version 2.0 # (the
+# "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from abc import ABC
+from dataclasses import dataclass
+from time import perf_counter
+from typing import Any, Dict, List
+
+import requests
+
+
+@dataclass(frozen=True, slots=True)
+class Response:
+    url: str
+    status_code: int
+    duration_millis: int
+    json_data: List[Any] | Dict[str, Any]
+
+
+class AbstractClient(ABC):
+
+    def __init__(self, base_url: str) -> None:
+        self._base_url = base_url
+
+    def _get_request(self, path: str, params: Dict[str, str] = None) -> Response:
+        start_time = perf_counter()
+        response = requests.get(url=f"{self._base_url}{path}", params=params)
+        duration_sec = perf_counter() - start_time
+        return Response(
+            url=response.request.url,
+            status_code=response.status_code,
+            duration_millis=round(1000 * duration_sec),
+            json_data=response.json()
+        )
--- test-automation/non-functional/query_service_client.py
+++ test-automation/non-functional/query_service_client.py
@@ -1,33 +1,29 @@
-from dataclasses import dataclass
-from time import perf_counter
-from typing import Any, Dict, List
+#
+# Copyright 2023 Jaroslav Chmurny
+#
+# This file is part of City Navigator.
+#
+# City Navigator is free software developed for educational and experimental
+# purposes. It is licensed under the Apache License, Version 2.0 # (the
+# "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-import requests
+from abstract_client import AbstractClient, Response
 
 
-@dataclass(frozen=True, slots=True)
-class Response:
-    url: str
-    status_code: int
-    duration_millis: int
-    json_data: List[Any] | Dict[str, Any]
-
-
-class QueryServiceClient:
+class QueryServiceClient(AbstractClient):
 
     def __init__(self, base_url: str) -> None:
-        self._base_url = base_url
-
-    def _get_request(self, path: str, params: Dict[str, str] = None) -> Response:
-        start_time = perf_counter()
-        response = requests.get(url=f"{self._base_url}{path}", params=params)
-        duration_sec = perf_counter() - start_time
-        return Response(
-            url=response.request.url,
-            status_code=response.status_code,
-            duration_millis=round(1000 * duration_sec),
-            json_data=response.json()
-        )
+        super().__init__(base_url)
 
     def get_means_of_transport_list(self) -> Response:
         return self._get_request("/means-of-transport")
@@ -49,7 +45,7 @@ class QueryServiceClient:
 
 
 def main() -> None:
-    client = QueryServiceClient("http://localhost:8000")
+    client = QueryServiceClient("http://localhost/city-navigator/api/query")
 
     print("Stations")
     for _ in range(20):
@@ -65,11 +61,13 @@ def main() -> None:
     print()
     print("Jounrey plans")
     journeys = [
-        ("Simmering", "Strebersdorf"),
+        ("Simmering", "Albertinaplatz"),
         ("Karlsplatz", "Alte Donau"),
         ("Am Tabor", "Herrengasse"),
         ("Schoenbrunn", "Heiligenstadt"),
         ("Penzing", "Rauscherstrasse"),
+        ("Krottenbachstrasse", "Heiligenstadt"),
+        ("Ottakring", "Sorgenthalgasse")
     ]
     for start, destination in journeys:
         response = client.search_journey_plan(start, destination)
