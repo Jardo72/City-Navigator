e80e75f90f8bd5cf5135c9f94e70ab96da79dfc6
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2023-04-07 10:41:10 +0200

    First steps towards package structre for test automation.

---
+++ test-automation/non-functional/statistics/collector.py
@@ -0,0 +1,55 @@
+#
+# Copyright 2023 Jaroslav Chmurny
+#
+# This file is part of City Navigator.
+#
+# City Navigator is free software developed for educational and experimental
+# purposes. It is licensed under the Apache License, Version 2.0 # (the
+# "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from rest import Response
+
+from .summary import Summary
+
+class Collector:
+
+    def __init__(self) -> None:
+        self._success_count: int = 0
+        self._client_error_count: int = 0
+        self._server_error_count: int = 0
+        self._overall_success_duration_millis: int = 0
+        self._min_success_duration_millis: int = 1000000
+        self._max_success_duration_millis: int = 0
+
+    def add(self, response: Response) -> None:
+        if 200 <= response.status_code < 300:
+            self._success_count += 1
+            self._overall_success_duration_millis += response.duration_millis
+            if self._min_success_duration_millis > response.duration_millis:
+                self._min_success_duration_millis = response.duration_millis
+            if self._max_success_duration_millis < response.duration_millis:
+                self._max_success_duration_millis = response.duration_millis
+        if 400 <= response.status_code < 500:
+            self._client_error_count += 1
+        if 500 <= response.status_code < 600:
+            self._server_error_count += 1
+
+    def get_summary(self) -> Summary:
+        return Summary(
+            success_count=self._success_count,
+            client_error_count=self._client_error_count,
+            server_error_count=self._server_error_count,
+            overall_success_duration_millis=self._overall_success_duration_millis,
+            min_success_duration_millis=self._min_success_duration_millis,
+            max_success_duration_millis=self._max_success_duration_millis
+        )
---
+++ test-automation/non-functional/statistics/summary.py
@@ -0,0 +1,47 @@
+#
+# Copyright 2023 Jaroslav Chmurny
+#
+# This file is part of City Navigator.
+#
+# City Navigator is free software developed for educational and experimental
+# purposes. It is licensed under the Apache License, Version 2.0 # (the
+# "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True, slots=True)
+class Summary:
+    success_count: int
+    client_error_count: int
+    server_error_count: int
+    overall_success_duration_millis: int
+    min_success_duration_millis: int
+    max_success_duration_millis: int
+
+    @property
+    def avg_success_duration_millis(self) -> int:
+        return round(self.overall_success_duration_millis / self.success_count)
+
+    def __add__(self, other: TestThreadSummary) -> TestThreadSummary:
+        assert isinstance(other, TestThreadSummary)
+        return TestThreadSummary(
+            success_count=self.success_count + other.success_count,
+            client_error_count=self.client_error_count + other.client_error_count,
+            server_error_count=self.server_error_count + other.server_error_count,
+            overall_success_duration_millis=self.overall_success_duration_millis + other.overall_success_duration_millis,
+            min_success_duration_millis=min(self.min_success_duration_millis, other.min_success_duration_millis),
+            max_success_duration_millis=max(self.max_success_duration_millis, other.max_success_duration_millis)
+        )
