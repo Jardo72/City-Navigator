68864cbb9f877c77d6f057c917899196da3e22f2
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2023-04-30 17:15:08 +0200

    Histogram (request duration) incorporated into master data service.

--- application/master-data-service/src/rest/router.py
+++ application/master-data-service/src/rest/router.py
@@ -22,7 +22,7 @@ from typing import List
 from uuid import uuid4
 
 from fastapi import Depends, APIRouter, status
-from prometheus_client import Counter
+from prometheus_client import Counter, Histogram
 from sqlalchemy.orm import Session
 
 from db import Edge, Line, MeansOfTransport, Station
@@ -42,10 +42,17 @@ _logger = getLogger("rest")
 
 
 router = APIRouter()
+
 request_counter = Counter(
     name="master_data_service_http_requests_total",
     documentation="Number of HTTP requests processed by master data service",
     labelnames=["method", "path"]
+)
+request_duration_histogram = Histogram(
+    name="master_data_service_http_request_duration_seconds",
+    documentation="Duration of HTTP requests processed by master data service (in seconds)",
+    labelnames=["method", "path"],
+    buckets=[0.25, 0.5, 0.75, 1, "+Inf"]
 )
 
 
@@ -55,8 +62,9 @@ async def get_means_of_transport_list(db: Session = Depends(get_db)):
     Provides a list with details of all means of transport.
     """
     request_counter.labels(method="GET", path="/means-of-transport").inc()
-    result_set = db.query(MeansOfTransport).order_by(MeansOfTransport.identifier).all()
-    return [as_means_of_transport_dto(record) for record in result_set]
+    with request_duration_histogram.labels(method="GET", path="/means-of-transport").time():
+        result_set = db.query(MeansOfTransport).order_by(MeansOfTransport.identifier).all()
+        return [as_means_of_transport_dto(record) for record in result_set]
 
 
 @router.get("/means-of-transport/{uuid}", response_model=MeansOfTransportDetails)
@@ -65,10 +73,11 @@ async def get_means_of_transport(uuid: str, db: Session = Depends(get_db)):
     Provides the details of the means of transport with the given UUID.
     """
     request_counter.labels(method="GET", path="/means-of-transport/{uuid}").inc()
-    record = db.query(MeansOfTransport).filter(MeansOfTransport.uuid == uuid).first()
-    if record is None:
-        raise means_of_transport_not_found_exception(uuid)
-    return as_means_of_transport_dto(record)
+    with request_duration_histogram.labels(method="GET", path="/means-of-transport/{uuid}").time():
+        record = db.query(MeansOfTransport).filter(MeansOfTransport.uuid == uuid).first()
+        if record is None:
+            raise means_of_transport_not_found_exception(uuid)
+        return as_means_of_transport_dto(record)
 
 
 @router.post("/means-of-transport", response_model=MeansOfTransportDetails, status_code=status.HTTP_201_CREATED)
@@ -81,14 +90,15 @@ async def create_means_of_transport(
     Creates a new means of transport using the given request data.
     """
     request_counter.labels(method="POST", path="/means-of-transport").inc()
-    means_of_transport = MeansOfTransport()
-    means_of_transport.uuid = str(uuid4())
-    means_of_transport.identifier = request.identifier
-    db.add(means_of_transport)
-    db.commit()
-    _logger.debug("New means of transport (uuid = %s) inserted into the database", means_of_transport.uuid)
-    notifier.send_notification(EventType.CREATED, MeansOfTransport, means_of_transport.uuid)
-    return as_means_of_transport_dto(means_of_transport)
+    with request_duration_histogram.labels(method="POST", path="/means-of-transport").time():
+        means_of_transport = MeansOfTransport()
+        means_of_transport.uuid = str(uuid4())
+        means_of_transport.identifier = request.identifier
+        db.add(means_of_transport)
+        db.commit()
+        _logger.debug("New means of transport (uuid = %s) inserted into the database", means_of_transport.uuid)
+        notifier.send_notification(EventType.CREATED, MeansOfTransport, means_of_transport.uuid)
+        return as_means_of_transport_dto(means_of_transport)
 
 
 @router.put("/means-of-transport/{uuid}")
@@ -102,14 +112,15 @@ async def update_means_of_transport(
     Updates the means of transport with the given UUID, using the given request data.
     """
     request_counter.labels(method="PUT", path="/means-of-transport/{uuid}").inc()
-    record = db.query(MeansOfTransport).filter(MeansOfTransport.uuid == uuid).first()
-    if record is None:
-        raise means_of_transport_not_found_exception(uuid)
-    record.identifier = request.identifier
-    db.commit()
-    _logger.debug("Means of transport (uuid = %s) updated in the database", uuid)
-    notifier.send_notification(EventType.UPDATED, MeansOfTransport, uuid)
-    return as_means_of_transport_dto(record)
+    with request_duration_histogram.labels(method="PUT", path="/means-of-transport/{uuid}").time():
+        record = db.query(MeansOfTransport).filter(MeansOfTransport.uuid == uuid).first()
+        if record is None:
+            raise means_of_transport_not_found_exception(uuid)
+        record.identifier = request.identifier
+        db.commit()
+        _logger.debug("Means of transport (uuid = %s) updated in the database", uuid)
+        notifier.send_notification(EventType.UPDATED, MeansOfTransport, uuid)
+        return as_means_of_transport_dto(record)
 
 
 @router.delete("/means-of-transport/{uuid}")
@@ -122,13 +133,14 @@ async def delete_means_of_transport(
     Deletes the means of transport with the given UUID.
     """
     request_counter.labels(method="DELETE", path="/means-of-transport/{uuid}").inc()
-    record = db.query(MeansOfTransport).filter(MeansOfTransport.uuid == uuid).first()
-    if record is None:
-        raise means_of_transport_not_found_exception(uuid)
-    db.delete(record)
-    db.commit()
-    _logger.debug("Means of transport deleted from the database")
-    notifier.send_notification(EventType.DELETED, MeansOfTransport, uuid)
+    with request_duration_histogram.labels(method="DELETE", path="/means-of-transport/{uuid}").time():
+        record = db.query(MeansOfTransport).filter(MeansOfTransport.uuid == uuid).first()
+        if record is None:
+            raise means_of_transport_not_found_exception(uuid)
+        db.delete(record)
+        db.commit()
+        _logger.debug("Means of transport deleted from the database")
+        notifier.send_notification(EventType.DELETED, MeansOfTransport, uuid)
 
 
 @router.get("/stations", response_model=List[StationDetails])
@@ -138,8 +150,9 @@ async def get_station_list(db: Session = Depends(get_db)):
     the order is ascending.
     """
     request_counter.labels(method="GET", path="/stations").inc()
-    result_set = db.query(Station).order_by(Station.name).all()
-    return [as_station_details_dto(record) for record in result_set]
+    with request_duration_histogram.labels(method="GET", path="/stations").time():
+        result_set = db.query(Station).order_by(Station.name).all()
+        return [as_station_details_dto(record) for record in result_set]
 
 
 @router.get("/station/{uuid}", response_model=StationDetails)
@@ -148,12 +161,14 @@ async def get_station(uuid: str, db: Session = Depends(get_db)):
     Provides the details of the station with the given UUID.
     """
     request_counter.labels(method="GET", path="/station/{uuid}").inc()
-    record = db.query(Station).filter(Station.uuid == uuid).first()
-    if record is None:
-        raise station_not_found_exception(uuid)
-    return as_station_details_dto(record)
+    with request_duration_histogram.labels(method="GET", path="/station/{uuid}").time():
+        record = db.query(Station).filter(Station.uuid == uuid).first()
+        if record is None:
+            raise station_not_found_exception(uuid)
+        return as_station_details_dto(record)
 
 
+@request_duration_histogram.labels(method="POST", path="/station").time()
 @router.post("/station", response_model=StationDetails, status_code=status.HTTP_201_CREATED)
 async def create_station(
     request: StationRequest,
@@ -164,14 +179,15 @@ async def create_station(
     Creates a new station using the given request data.
     """
     request_counter.labels(method="POST", path="/station").inc()
-    station = Station()
-    station.uuid = str(uuid4())
-    station.name = request.name
-    db.add(station)
-    db.commit()
-    _logger.debug("New station (uuid = %s) inserted into the database", station.uuid)
-    notifier.send_notification(EventType.CREATED, Station, station.uuid)
-    return as_station_details_dto(station)
+    with request_duration_histogram.labels(method="POST", path="/station").time():
+        station = Station()
+        station.uuid = str(uuid4())
+        station.name = request.name
+        db.add(station)
+        db.commit()
+        _logger.debug("New station (uuid = %s) inserted into the database", station.uuid)
+        notifier.send_notification(EventType.CREATED, Station, station.uuid)
+        return as_station_details_dto(station)
 
 
 @router.put("/station/{uuid}", response_model=StationDetails)
@@ -185,13 +201,14 @@ async def update_station(
     Updates the station with the given UUID, using the given request data.
     """
     request_counter.labels(method="PUT", path="/station/{uuid}").inc()
-    record = db.query(Station).filter(Station.uuid == uuid).first()
-    if record is None:
-        raise station_not_found_exception(uuid)
-    record.name = request.name
-    db.commit()
-    notifier.send_notification(EventType.UPDATED, Station, uuid)
-    return as_station_details_dto(record)
+    with request_duration_histogram.labels(method="PUT", path="/station/{uuid}").time():
+        record = db.query(Station).filter(Station.uuid == uuid).first()
+        if record is None:
+            raise station_not_found_exception(uuid)
+        record.name = request.name
+        db.commit()
+        notifier.send_notification(EventType.UPDATED, Station, uuid)
+        return as_station_details_dto(record)
 
 
 @router.delete("/station/{uuid}")
@@ -204,19 +221,21 @@ async def delete_station(
     Deletes the station with the given UUID.
     """
     request_counter.labels(method="DELETE", path="/station/{uuid}").inc()
-    record = db.query(Station).filter(Station.uuid == uuid).first()
-    if record is None:
-        raise station_not_found_exception(uuid)
-    db.delete(record)
-    db.commit()
-    notifier.send_notification(EventType.DELETED, Station, uuid)
+    with request_duration_histogram.labels(method="DELETE", path="/station/{uuid}").time():
+        record = db.query(Station).filter(Station.uuid == uuid).first()
+        if record is None:
+            raise station_not_found_exception(uuid)
+        db.delete(record)
+        db.commit()
+        notifier.send_notification(EventType.DELETED, Station, uuid)
 
 
 @router.get("/lines", response_model=List[LineInfo])
 async def get_lines(db: Session = Depends(get_db)):
     request_counter.labels(method="GET", path="/lines").inc()
-    result_set = db.query(Line).order_by(Line.label).all()
-    return [as_line_info_dto(record) for record in result_set]
+    with request_duration_histogram.labels(method="GET", path="/lines").time():
+        result_set = db.query(Line).order_by(Line.label).all()
+        return [as_line_info_dto(record) for record in result_set]
 
 
 @router.get("/line/{uuid}", response_model=LineDetails)
@@ -225,10 +244,11 @@ async def get_line(uuid: str, db: Session = Depends(get_db)):
     Provides the details of the line with the given UUID.
     """
     request_counter.labels(method="GET", path="/line/{uuid}").inc()
-    record = db.query(Line).filter(Line.uuid == uuid).first()
-    if record is None:
-        raise line_not_found_exception(uuid)
-    return as_line_details_dto(record)
+    with request_duration_histogram.labels(method="GET", path="/line/{uuid}").time():
+        record = db.query(Line).filter(Line.uuid == uuid).first()
+        if record is None:
+            raise line_not_found_exception(uuid)
+        return as_line_details_dto(record)
 
 
 @router.post("/line", response_model=LineDetails, status_code=status.HTTP_201_CREATED)
@@ -241,17 +261,18 @@ async def create_line(
     Creates a new line using the given request data.
     """
     request_counter.labels(method="POST", path="/line").inc()
-    line = Line()
-    line.uuid = str(uuid4())
-    update_line_entity_from_dto(entity=line, dto=request)
-    itinerary_one, itinerary_two = create_edges_from_dto(dto=request, line_uuid=line.uuid)
-    _logger.debug("%d edges in itinerary one, %d edges in itinerary two", len(itinerary_one), len(itinerary_two))
-    db.add(line)
-    for single_edge in itinerary_one + itinerary_two:
-        db.add(single_edge)
-    db.commit()
-    notifier.send_notification(EventType.CREATED, Line, line.uuid)
-    return as_line_details_dto(line)
+    with request_duration_histogram.labels(method="POST", path="/line").time():
+        line = Line()
+        line.uuid = str(uuid4())
+        update_line_entity_from_dto(entity=line, dto=request)
+        itinerary_one, itinerary_two = create_edges_from_dto(dto=request, line_uuid=line.uuid)
+        _logger.debug("%d edges in itinerary one, %d edges in itinerary two", len(itinerary_one), len(itinerary_two))
+        db.add(line)
+        for single_edge in itinerary_one + itinerary_two:
+            db.add(single_edge)
+        db.commit()
+        notifier.send_notification(EventType.CREATED, Line, line.uuid)
+        return as_line_details_dto(line)
 
 
 @router.put("/line/{uuid}", response_model=LineDetails)
@@ -265,19 +286,20 @@ async def update_line(
     Updates the the line with the given UUID, using the given request data.
     """
     request_counter.labels(method="PUT", path="/line/{uuid}").inc()
-    record = db.query(Line).filter(Line.uuid == uuid).first()
-    if record is None:
-        raise line_not_found_exception(uuid)
-    update_line_entity_from_dto(entity=record, dto=request)
-    itinerary_one, itinerary_two = create_edges_from_dto(dto=request, line_uuid=uuid)
-    db.query(Edge).filter(Edge.line_uuid == uuid).delete()
-    _logger.debug("Original itineraries deleted")
-    for single_edge in itinerary_one + itinerary_two:
-        db.add(single_edge)
-    _logger.debug("Modified itineraries created")
-    db.commit()
-    notifier.send_notification(EventType.UPDATED, Line, uuid)
-    return as_line_details_dto(record)
+    with request_duration_histogram.labels(method="PUT", path="/line/{uuid}").time():
+        record = db.query(Line).filter(Line.uuid == uuid).first()
+        if record is None:
+            raise line_not_found_exception(uuid)
+        update_line_entity_from_dto(entity=record, dto=request)
+        itinerary_one, itinerary_two = create_edges_from_dto(dto=request, line_uuid=uuid)
+        db.query(Edge).filter(Edge.line_uuid == uuid).delete()
+        _logger.debug("Original itineraries deleted")
+        for single_edge in itinerary_one + itinerary_two:
+            db.add(single_edge)
+        _logger.debug("Modified itineraries created")
+        db.commit()
+        notifier.send_notification(EventType.UPDATED, Line, uuid)
+        return as_line_details_dto(record)
 
 
 @router.delete("/line/{uuid}")
@@ -291,9 +313,10 @@ async def delete_line(
     for the concerned line (both directions).
     """
     request_counter.labels(method="DELETE", path="/line/{uuid}").inc()
-    record = db.query(Line).filter(Line.uuid == uuid).first()
-    if record is None:
-        raise line_not_found_exception(uuid)
-    db.delete(record)
-    db.commit()
-    notifier.send_notification(EventType.DELETED, Line, uuid)
+    with request_duration_histogram.labels(method="DELETE", path="/line/{uuid}").time():
+        record = db.query(Line).filter(Line.uuid == uuid).first()
+        if record is None:
+            raise line_not_found_exception(uuid)
+        db.delete(record)
+        db.commit()
+        notifier.send_notification(EventType.DELETED, Line, uuid)
