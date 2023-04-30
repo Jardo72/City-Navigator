8c14f3f5cc72d9e82af11648592dc6f5681e93e9
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2023-04-30 11:12:07 +0200

    Histogram for query service working properly.

--- application/query-service/src/rest/router.py
+++ application/query-service/src/rest/router.py
@@ -46,22 +46,22 @@ request_counter = Counter(
 request_duration_histogram = Histogram(
     name="query_service_http_request_duration_seconds",
     documentation="Duration of HTTP requests processed by query service (in seconds)",
-    labelnames=["method", "path"]
+    labelnames=["method", "path"],
+    buckets=[0.25, 0.5, 0.75, 1, 1.5, "+Inf"]
 )
 
 
-@request_duration_histogram.labels(method="GET", path="/means-of-transport").time()
 @router.get("/means-of-transport", response_model=List[MeansOfTransportDetails])
 async def get_means_of_transport(db: Session = Depends(get_db)):
     """
     Provides a list of all means of transport.
     """
     request_counter.labels(method="GET", path="/means-of-transport").inc()
-    means_of_transport = db.query(MeansOfTransport).all()
-    return [as_means_of_transport_details(single_means_of_transport) for single_means_of_transport in means_of_transport]
+    with request_duration_histogram.labels(method="GET", path="/means-of-transport").time():
+        means_of_transport = db.query(MeansOfTransport).all()
+        return [as_means_of_transport_details(single_means_of_transport) for single_means_of_transport in means_of_transport]
 
 
-@request_duration_histogram.labels(method="GET", path="/stations").time()
 @router.get("/stations", response_model=List[StationDetails])
 async def get_station_list(filter: str = None, db: Session = Depends(get_db)):
     """
@@ -71,30 +71,30 @@ async def get_station_list(filter: str = None, db: Session = Depends(get_db)):
     will be returned.
     """
     request_counter.labels(method="GET", path="/stations").inc()
-    _logger.debug("Asked for stations - filter = %s", filter)
-    if filter is None:
-        stations = db.query(Station).order_by(Station.name).all()
-    else:
-        filter = filter.replace("*", "%")
-        stations = db.query(Station).filter(Station.name.like(filter)).order_by(Station.name).all()
-    return [as_station_details(single_station) for single_station in stations]
+    with request_duration_histogram.labels(method="GET", path="/stations").time():
+        _logger.debug("Asked for stations - filter = %s", filter)
+        if filter is None:
+            stations = db.query(Station).order_by(Station.name).all()
+        else:
+            filter = filter.replace("*", "%")
+            stations = db.query(Station).filter(Station.name.like(filter)).order_by(Station.name).all()
+        return [as_station_details(single_station) for single_station in stations]
 
 
-@request_duration_histogram.labels(method="GET", path="/station").time()
 @router.get("/station", response_model=StationDetails)
 async def get_station_details(name: str, db: Session = Depends(get_db)):
     """
     Provides the details of the station with the given name.
     """
     request_counter.labels(method="GET", path="/station").inc()
-    _logger.debug("Asked for single station - name = %s", name)
-    station = db.query(Station).filter(Station.name == name).first()
-    if station is None:
-        raise station_not_found_exception(name)
-    return as_station_details(station)
+    with request_duration_histogram.labels(method="GET", path="/station").time():
+        _logger.debug("Asked for single station - name = %s", name)
+        station = db.query(Station).filter(Station.name == name).first()
+        if station is None:
+            raise station_not_found_exception(name)
+        return as_station_details(station)
 
 
-@request_duration_histogram.labels(method="GET", path="/lines").time()
 @router.get("/lines", response_model=List[LineInfo])
 async def get_line_list(means_of_transport: str = None, db: Session = Depends(get_db)):
     """
@@ -102,40 +102,41 @@ async def get_line_list(means_of_transport: str = None, db: Session = Depends(ge
     (i.e. if the parameter is omitted), all lines are returned.
     """
     request_counter.labels(method="GET", path="/lines").inc()
-    if means_of_transport is None:
-        lines = db.query(Line).order_by(Line.label).all()
-    else:
-        filter = db.query(MeansOfTransport).filter(MeansOfTransport.identifier == means_of_transport).first()
-        lines = db.query(Line).filter(Line.means_of_transport == filter).order_by(Line.label).all()
-    return [as_line_info(single_line) for single_line in lines]
+    with request_duration_histogram.labels(method="GET", path="/lines").time():
+        if means_of_transport is None:
+            lines = db.query(Line).order_by(Line.label).all()
+        else:
+            filter = db.query(MeansOfTransport).filter(MeansOfTransport.identifier == means_of_transport).first()
+            lines = db.query(Line).filter(Line.means_of_transport == filter).order_by(Line.label).all()
+        return [as_line_info(single_line) for single_line in lines]
 
 
-@request_duration_histogram.labels(method="GET", path="/line").time()
 @router.get("/line", response_model=LineDetails)
 async def get_line_details(label: str, db: Session = Depends(get_db)):
     """
     Provides the details of the line with the given label.
     """
     request_counter.labels(method="GET", path="/line").inc()
-    line = db.query(Line).filter(Line.label == label).first()
-    if not line:
-        raise line_not_found_exception(label)
-    return as_line_details(line)
+    with request_duration_histogram.labels(method="GET", path="/line").time():
+        line = db.query(Line).filter(Line.label == label).first()
+        if not line:
+            raise line_not_found_exception(label)
+        return as_line_details(line)
 
 
-@request_duration_histogram.labels(method="GET", path="/journey-plan").time()
 @router.get("/journey-plan", response_model=JourneyPlan)
 async def search_journey_plan(start: str, destination: str, db: Session = Depends(get_db)):
     """
     Finds and returns a journey plan with the given start and destination.
     """
-    _logger.debug("Asked to search path from %s to %s", start, destination)
     request_counter.labels(method="GET", path="/journey-plan").inc()
-    start_station = db.query(Station).filter(Station.name == start).first()
-    if start_station is None:
-        raise station_not_found_exception(start)
-    destination_station = db.query(Station).filter(Station.name == destination).first()
-    if destination_station is None:
-        raise station_not_found_exception(destination)
-    search_result = find_shortest_path(start_station, destination_station)
-    return as_journey_plan(search_result)
+    with request_duration_histogram.labels(method="GET", path="/journey-plan").time():
+        _logger.debug("Asked to search path from %s to %s", start, destination)
+        start_station = db.query(Station).filter(Station.name == start).first()
+        if start_station is None:
+            raise station_not_found_exception(start)
+        destination_station = db.query(Station).filter(Station.name == destination).first()
+        if destination_station is None:
+            raise station_not_found_exception(destination)
+        search_result = find_shortest_path(start_station, destination_station)
+        return as_journey_plan(search_result)
