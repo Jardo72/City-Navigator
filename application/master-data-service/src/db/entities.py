71686a8b25d93b26682cef48886c236d00e94439
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2023-05-16 08:16:19 +0200

    Correction in SQLAlchemy entities for master data service.

--- application/master-data-service/src/db/entities.py
+++ application/master-data-service/src/db/entities.py
@@ -42,10 +42,10 @@ class Line(Base):
     __tablename__ = "T_LINES"
     uuid = Column("UUID", String, primary_key=True, index=True)
     label = Column("LABEL", String, nullable=False)
-    means_of_transport_uuid = Column("MEANS_OF_TRANSPORT_UUID", String, ForeignKey("MEANS_OF_TRANSPORT.UUID"), nullable=False)
+    means_of_transport_uuid = Column("MEANS_OF_TRANSPORT_UUID", String, ForeignKey("T_MEANS_OF_TRANSPORT.UUID"), nullable=False)
     means_of_transport = relationship("MeansOfTransport", foreign_keys="Line.means_of_transport_uuid")
-    terminal_stop_one_uuid = Column("TERMINAL_STOP_ONE_UUID", String, ForeignKey("STATIONS.UUID"), nullable=False)
-    terminal_stop_two_uuid = Column("TERMINAL_STOP_TWO_UUID", String, ForeignKey("STATIONS.UUID"), nullable=False)
+    terminal_stop_one_uuid = Column("TERMINAL_STOP_ONE_UUID", String, ForeignKey("T_STATIONS.UUID"), nullable=False)
+    terminal_stop_two_uuid = Column("TERMINAL_STOP_TWO_UUID", String, ForeignKey("T_STATIONS.UUID"), nullable=False)
     terminal_stop_one = relationship("Station", foreign_keys="Line.terminal_stop_one_uuid")
     terminal_stop_two = relationship("Station", foreign_keys="Line.terminal_stop_two_uuid")
 
@@ -54,9 +54,9 @@ class Edge(Base):
     __tablename__ = "T_EDGES"
     uuid = Column("UUID", String, primary_key=True)
     distance_min = Column("DISTANCE_MIN", Integer, nullable=False)
-    start_station_uuid = Column("START_STATION_UUID", String, ForeignKey("STATIONS.UUID"), nullable=False)
-    end_station_uuid = Column("END_STATION_UUID", String, ForeignKey("STATIONS.UUID"), nullable=False)
-    line_uuid = Column("LINE_UUID", String, ForeignKey("LINES.UUID"), nullable=False)
+    start_station_uuid = Column("START_STATION_UUID", String, ForeignKey("T_STATIONS.UUID"), nullable=False)
+    end_station_uuid = Column("END_STATION_UUID", String, ForeignKey("T_STATIONS.UUID"), nullable=False)
+    line_uuid = Column("LINE_UUID", String, ForeignKey("T_LINES.UUID"), nullable=False)
     start_station = relationship("Station", foreign_keys="Edge.start_station_uuid", back_populates="outbound_edges")
     end_station = relationship("Station", foreign_keys="Edge.end_station_uuid")
     line = relationship("Line", foreign_keys="Edge.line_uuid")
