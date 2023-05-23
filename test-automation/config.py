a37d74f0b4ad54313f47f6bc459c63ffa378c047
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2023-05-23 19:32:10 +0200

    Test automation - first steps towards generation of client errors.

--- test-automation/config.py
+++ test-automation/config.py
@@ -26,8 +26,11 @@ class Config:
     query_service_base_url: str
     test_duration_minutes: int
     journey_plan_search_threads: int
+    journey_plan_error_percentage: int
     line_query_threads: int
+    line_query_error_percentage: int
     station_query_threads: int
+    station_query_error_percentage: int
     station_filter_threads: int
 
     @property
@@ -45,7 +48,10 @@ def read_from_file(filename: str) -> Config:
             query_service_base_url=json_data["query_service_base_url"],
             test_duration_minutes=json_data["test_duration_minutes"],
             journey_plan_search_threads=json_data["journey_plan_search_threads"],
+            journey_plan_error_percentage=json_data.get("journey_plan_error_percentage", 0),
             line_query_threads=json_data["line_query_threads"],
+            line_query_error_percentage=json_data.get("line_query_error_percentage", 0),
             station_query_threads=json_data["station_query_threads"],
+            station_query_error_percentage=json_data.get("station_query_error_percentage", 0),
             station_filter_threads=json_data["station_filter_threads"]
         )
