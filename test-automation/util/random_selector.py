92099ca4c21d9ce5da76d0b334bb341b7f0103cc
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2023-05-25 18:39:01 +0200

    Generation of errors is starting to work.

--- test-automation/util/random_selector.py
+++ test-automation/util/random_selector.py
@@ -17,22 +17,48 @@
 # limitations under the License.
 #
 
+from enum import IntEnum
+from enum import auto, unique
 from random import randint
 from typing import Tuple
+
+
+@unique
+class _SelectionMode(IntEnum):
+    VALID = auto()
+    INVALID = auto()
+
 
 class RandomSelector:
 
     def __init__(self, values: Tuple[str], error_percentage: int = 0) -> None:
         self._values = values
         self._max_index = len(values) - 1
+        self._error_period = 100 / error_percentage if error_percentage > 0 else None
+        self._counter = 0
+
+    def _get_current_selection_mode(self) -> _SelectionMode:
+        self._counter += 1
+        if self._error_period is None:
+            return _SelectionMode.VALID
+        if (self._counter % self._error_period) == 0:
+            return _SelectionMode.INVALID
+        return _SelectionMode.VALID
+
+    def _random_value(self, selection_mode: _SelectionMode) -> str:
+        if selection_mode is _SelectionMode.VALID:
+            random_index = randint(0, self._max_index)
+            return self._values[random_index]
+        return "NoSuchValue"
 
     def random_value(self) -> str:
-        random_index = randint(0, self._max_index)
-        return self._values[random_index]
+        selection_mode = self._get_current_selection_mode()
+        return self._random_value(selection_mode)
 
     def random_pair(self) -> Tuple[str, str]:
-        a = self.random_value()
-        b = self.random_value()
+        selection_mode = self._get_current_selection_mode()
+        a = self._random_value(selection_mode)
+        b = self._random_value(_SelectionMode.VALID)
         while a == b:
-            b = self.random_value()
+            b = self._random_value(_SelectionMode.VALID)
         return (a, b)
