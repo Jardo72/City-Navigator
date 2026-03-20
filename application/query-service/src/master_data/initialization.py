82bfbdf11785a6cda7cab21cb7aba0307eb7c2e8
Jaroslav Chmurny <jaroslav.chmurny@gmail.com>
2026-03-20 10:00:43 +0100

    Initialization from master data improved.

--- application/query-service/src/master_data/initialization.py
+++ application/query-service/src/master_data/initialization.py
@@ -89,7 +89,7 @@ def _retrieve_line_details(uuid: str) -> LineDetailsMaster:
 
 
 def _retrieve_from_master_data_service() -> RetrievalResult:
-    TIMEOUT_SEC = 15
+    TIMEOUT_SEC = 150
     with ThreadPoolExecutor(max_workers=6) as executor:
         lines_future = executor.submit(_retrieve_lines)
         means_of_transport_future = executor.submit(_retrieve_means_of_transport)
